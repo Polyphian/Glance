3e5e88d5763a1b9a67a8da064dcbec26884a915b
Polyphian <48692915+Polyphian@users.noreply.github.com>
2022-10-02 21:04:29 -0400

    Dark Thin

---
+++ Themes/Modern/14P/Dark Thin/1/theme.js
@@ -0,0 +1,27 @@
+
+//Greeting
+ctx.font = 'bold 57px -apple-system';
+ctx.textAlign = "left";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$GREETING$', 80, 1914, 998);
+
+//Line1
+ctx.font = 'bold 39px -apple-system';
+ctx.textAlign = "left";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$STRING1$', 226, 2044, 870);
+
+//Line2
+ctx.font = 'normal 39px -apple-system';
+ctx.textAlign = "left";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$STRING2$', 226, 2092, 870);
---
+++ Themes/Modern/14P/Dark/5/theme.js
@@ -0,0 +1,45 @@
+//City
+ctx.font = 'normal 57px -apple-system';
+ctx.textAlign = "left";
+ctx.textBaseline = "top";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$CITY$', 173, 699, 590);
+
+//HighLowTemps
+ctx.font = '34px -apple-system';
+ctx.textAlign = "right";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$TEMPS$', 990, 713, 200);
+
+//Greeting
+ctx.font = 'bold 57px -apple-system';
+ctx.textAlign = "left";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$GREETING$', 94, 795, 998);
+
+//Line1
+ctx.font = 'bold 39px -apple-system';
+ctx.textAlign = "left";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$STRING1$', 94, 873, 998);
+
+//Line2
+ctx.font = 'normal 39px -apple-system';
+ctx.textAlign = "left";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$STRING2$', 94, 921, 998);
