c323bea1cf5c71fea1c2137a7953b08548bc63e9
Polyphian <48692915+Polyphian@users.noreply.github.com>
2022-10-09 19:42:02 -0400

    lightthin

---
+++ Themes/Modern/14PM/Light Thin/3/theme.js
@@ -0,0 +1,27 @@
+ctx.translate(0, 40);
+//Greeting
+ctx.font = 'bold 64px -apple-system';
+ctx.textAlign = "left";
+ctx.fillStyle = "#000000";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$GREETING$', 80, 1520, 998);
+
+//Line1
+ctx.font = 'bold 43px -apple-system';
+ctx.textAlign = "left";
+ctx.fillStyle = "#000000";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$STRING1$', 226, 1650, 975);
+
+//Line2
+ctx.font = 'normal 39px -apple-system';
+ctx.textAlign = "left";
+ctx.fillStyle = "#000000";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$STRING2$', 226, 1698, 975);
