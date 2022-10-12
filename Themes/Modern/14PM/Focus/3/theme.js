a86c84ba4d025e918bf69f61c35fbee0b81c8e9f
Polyphian <48692915+Polyphian@users.noreply.github.com>
2022-10-12 14:53:36 -0400

    .

---
+++ Themes/Modern/14PM/Focus/3/theme.js
@@ -0,0 +1,45 @@
+//City
+ctx.font = 'normal 64px -apple-system';
+ctx.textAlign = "left";
+ctx.textBaseline = "top";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$CITY$', 211, 1430, 650);
+
+//HighLowTemps
+ctx.font = '37px -apple-system';
+ctx.textAlign = "right";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$TEMPS$', 1194, 1445, 260);
+
+//Greeting
+ctx.font = 'bold 64px -apple-system';
+ctx.textAlign = "left";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$GREETING$', 95, 1552, 1105);
+
+//Line1
+ctx.font = 'bold 43px -apple-system';
+ctx.textAlign = "left";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$STRING1$', 95, 1638, 1105);
+
+//Line2
+ctx.font = 'normal 39px -apple-system';
+ctx.textAlign = "left";
+ctx.fillStyle = "#ffffff";
+ctx.shadowOffsetY = 4;
+ctx.shadowColor = "rgba(0,0,0,0.1)";
+ctx.shadowBlur = 32;
+ctx.fillText('$STRING2$', 95, 1691, 1105);
